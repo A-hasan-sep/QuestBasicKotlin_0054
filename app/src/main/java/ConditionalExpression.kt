7a6415bf1d59ba3dbd1be9d9a985221937b2b958
hasanmuhammadridlo <hasan.m.ft20@mail.umy.ac.id>
2024-10-14 01:38:54 +0700

    commit 4 = memasukkan coding ConditionalExpression.kt bagian ConditionalStatement if-else dan penjelasan.

---
+++ app/src/main/java/ConditionalExpression.kt
@@ -0,0 +1,16 @@
+package com.example.test
+
+// conditional expression
+// conditional expression adalah ekspersi yang mengembalikan nilai berdasarkan kondisi tertentu
+// conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("number is positive")
+    } else {
+        println("number is negative")
+    }
+}
