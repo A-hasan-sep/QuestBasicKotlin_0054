f4d1dc9f65b78f17b7fe5539d3aa05f699865b9a
hasanmuhammadridlo <hasan.m.ft20@mail.umy.ac.id>
2024-10-14 02:39:45 +0700

    commit 12 = memasukkan coding Fungctions.kt, fun withNamedArgument dan penjelasannya.

--- app/src/main/java/Fungctions.kt
+++ app/src/main/java/Fungctions.kt
@@ -10,3 +10,11 @@ fun withParameter(name: String) {
     println("== With Parameter ==")
     println("Hello, $name")
 }
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameternya
+fun withNamedArgument(firstName: String, lastName: String, age: Int) {
+    println()
+    println("== withNameArgument ==")
+    println("Hello, $firstName $lastName you are $age years old")
+}
