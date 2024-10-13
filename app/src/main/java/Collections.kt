95b9f07d897000c80fbf5a328b8798b30ad4dc2c
hasanmuhammadridlo <hasan.m.ft20@mail.umy.ac.id>
2024-10-14 00:08:26 +0700

    commit 3 = memasukan coding ke dalam Collections.kt bagian ContohSet dan penjelasannya

--- app/src/main/java/Collections.kt
+++ app/src/main/java/Collections.kt
@@ -31,3 +31,32 @@ fun ContohList() {
     val shapeLocked: List<String> = shape
     println(shapeLocked)
 }
+
+// set adalah kumpulan data yang tidak disusun secara berurutan dan hanya menyimpan data yang unik(tidak ada duplikasi)
+// set bersifat unordered, artinya data yang dimasukkan tidak akan memiliki posisi yang sesuai urutan
+// set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// set read-only menggunakan setOf() sedangkan set mutable menggunakan mutableSetOf()
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    //set raed-only
+    val readOnlyAbjad = setOf("d", "e", "f")
+    println(readOnlyAbjad)
+
+    //set mutable
+    val shape: MutableSet<String> = mutableSetOf("persegi", "lingkaran", "segitiga")
+    println(shape)
+
+    // menambahkan data ke dalam set mutable
+    shape.add("lingkaranb")
+    println(shape)
+
+    // menghapus data dari dalam set mutable
+    shape.remove("segitiga")
+
+    //set read-only
+    val shapeLocked: Set<String> = shape
+    println(shapeLocked)
+}
